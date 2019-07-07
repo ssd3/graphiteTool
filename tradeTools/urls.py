d23a0bbc9ab24456d18e6316e18ba413b02ab838
vt1 <vt7vt.dev@gmail.com>
2019-07-07 17:16:17 +0300

    index page

--- tradeTools/urls.py
+++ tradeTools/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('', views.index)
 ]
